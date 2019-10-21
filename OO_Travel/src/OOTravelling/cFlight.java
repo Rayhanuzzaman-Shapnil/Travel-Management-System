df7dde0621834b0e595ff78c90018ecd7efd7a8e
Rayhanuzzaman Shapnil <rayhanuzzamanr@gmail.com>
2019-10-21 23:53:03 +0600

    Add files via upload

---
+++ OO_Travel/src/OOTravelling/cFlight.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OOTravelling;
+
+/**
+ *
+ * @author A
+ */
+class cFlight {
+
+public double eSylhet_Airport=22;
+public double eChittagong_Airport=25;
+public double eIndia_Airport=100;
+public double eShanghai_Airport=300;
+public double eToronto_Airport=600;
+public double eTokyo_Airport=500;
+public double eJakarta_Airport=260;
+public double eMoscow_Airport=700;
+public double eNew_York_Airport=800;
+public double eCanada_Airport=850;
+public double eBrazil_Airport=750;
+public double eArgentina_Airport=750;
+public double eBerlin_Airport=700;
+public double eMaldives_Airport=200;
+public double eSri_Lanka_Airport=150;
+public double eNepal_Airport=120;
+public double eBhutan_Airport=130;
+public double eTaiwan_Airport=200;
+public double eEgypt_Airport=580;
+//====================================================
+public double sSylhet_Airport=22+5;
+public double sChittagong_Airport=25+5;
+public double sIndia_Airport=100+10;
+public double sShanghai_Airport=300+10;
+public double sToronto_Airport=600+10;
+public double sTokyo_Airport=500+10;
+public double sJakarta_Airport=260+10;
+public double sMoscow_Airport=700+10;
+public double sNew_York_Airport=800+10;
+public double sCanada_Airport=850+10;
+public double sBrazil_Airport=750+10;
+public double sArgentina_Airport=750+10;
+public double sBerlin_Airport=700+10;
+public double sMaldives_Airport=200+10;
+public double sSri_Lanka_Airport=150+10;
+public double sNepal_Airport=120+10;
+public double sBhutan_Airport=130+10;
+public double sTaiwan_Airport=200+10;
+public double sEgypt_Airport=580+10;
+//=====================================================
+public double fSylhet_Airport=22+10;
+public double fChittagong_Airport=25+10;
+public double fIndia_Airport=100+20;
+public double fShanghai_Airport=300+20;
+public double fToronto_Airport=600+20;
+public double fTokyo_Airport=500+20;
+public double fJakarta_Airport=260+20;
+public double fMoscow_Airport=700+20;
+public double fNew_York_Airport=800+20;
+public double fCanada_Airport=850+20;
+public double fBrazil_Airport=750+20;
+public double fArgentina_Airport=750+20;
+public double fBerlin_Airport=700+20;
+public double fMaldives_Airport=200+20;
+public double fSri_Lanka_Airport=150+20;
+public double fNepal_Airport=120+20;
+public double fBhutan_Airport=130+20;
+public double fTaiwan_Airport=200+20;
+public double fEgypt_Airport=580+20;
+}
